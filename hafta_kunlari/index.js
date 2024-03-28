27220f420e7de18b28adc1964d82c75cf1316bbb
AbdugafforovMurodjon <ume8156@gamil.com>
2024-03-28 17:40:55 +0500

    hafta kunlarini chiqarish

---
+++ hafta_kunlari/index.js
@@ -0,0 +1,29 @@
+let son = parseInt(prompt("1 dan 7 gacha bo'lgan son kiriting:"));
+
+if (son === 1) {
+    console.log("Dushanba");
+} 
+
+else if (son === 2) {
+    console.log("Seshanba");
+} 
+
+else if (son === 3) {
+    console.log("Chorshanba");
+} 
+else if (son === 4) {
+    console.log("Payshanba");
+} 
+else if (son === 5) {
+    console.log("Juma");
+} 
+else if (son === 6) {
+    console.log("Shanba");
+} 
+else if (son === 7) {
+    console.log("Yakshanba");
+} 
+
+else {
+    console.log("Noto'g'ri qiymat kiritildi!");
+}
